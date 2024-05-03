3a1871b17d54f6db21c3043ae13e7563ca12a046
aliml92 <almowins5yearsss@gmail.com>
2024-05-03 21:06:13 +0500

    feat: add `ErrInvalidOrExpiredResetURL` error

--- auth/errors.go
+++ auth/errors.go
@@ -3,11 +3,12 @@ package auth
 import "errors"
 
 var (
-	ErrEmailAlreadyTaken  = errors.New("Email already taken")
-	ErrInvalidCredentials = errors.New("Invalid credentials")
-	ErrInvalidOTP         = errors.New("Invalid OTP")
-	ErrExpiredOTP         = errors.New("Expired OTP")
-	ErrEmailNotConfirmed  = errors.New("Email not confirmed")
-	ErrAccountBlocked     = errors.New("Account blocked")
-	ErrAccountInactive    = errors.New("Account inactive")
+	ErrEmailAlreadyTaken        = errors.New("Email already taken")
+	ErrInvalidCredentials       = errors.New("Invalid credentials")
+	ErrInvalidOTP               = errors.New("Invalid OTP")
+	ErrExpiredOTP               = errors.New("Expired OTP")
+	ErrEmailNotConfirmed        = errors.New("Email not confirmed")
+	ErrAccountBlocked           = errors.New("Account blocked")
+	ErrAccountInactive          = errors.New("Account inactive")
+	ErrInvalidOrExpiredResetURL = errors.New("Invalid or expired reset URL")
 )
