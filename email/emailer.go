f5b57322ee6d6aabc0a8383668958af30638fdd7
aliml92 <almowins5yearsss@gmail.com>
2024-07-12 20:21:19 +0500

    chore: rename email package

---
+++ email/emailer.go
@@ -0,0 +1,14 @@
+package email
+
+import "context"
+
+type Message struct {
+	Subject     string
+	To          string
+	HtmlContent string
+}
+
+type Emailer interface {
+	NewMessage(subject string, to string, tmplName string, data interface{}) (Message, error)
+	Send(ctx context.Context, message Message) error
+}
