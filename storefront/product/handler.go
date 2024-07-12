f0ce82ee95dd29d11b5d583633cb458fa29a57f7
aliml92 <almowins5yearsss@gmail.com>
2024-07-12 16:50:04 +0500

    chore: add Render method

--- storefront/product/handler.go
+++ storefront/product/handler.go
@@ -1,42 +1,22 @@
 package product
 
 import (
+	"context"
 	"fmt"
+	homepage "github.com/aliml92/anor/html/dtos/pages/home"
+	notfoundpage "github.com/aliml92/anor/html/dtos/pages/not_found"
+	productdetailspage "github.com/aliml92/anor/html/dtos/pages/product_details"
+	searchlistings "github.com/aliml92/anor/html/dtos/pages/search_listings"
+	"github.com/aliml92/anor/html/dtos/partials"
 	"github.com/aliml92/anor/redis/cache/session"
 	"github.com/aliml92/anor/search"
-	"github.com/pkg/errors"
 	"log/slog"
 	"net/http"
+	"path"
 
 	"github.com/aliml92/anor"
 	"github.com/aliml92/anor/html"
-	"github.com/aliml92/anor/pkg/httperrors"
 )
-
-var ErrInvalidHandle = errors.New("invalid handle value")
-
-type BindValidator interface {
-	Binder
-	Validator
-}
-
-type Binder interface {
-	Bind(r *http.Request) error
-}
-
-type Validator interface {
-	Validate() error
-}
-
-func bindValid[T BindValidator](r *http.Request, v T) error {
-	if err := v.Bind(r); err != nil {
-		return fmt.Errorf("bind request: %w", err)
-	}
-	if err := v.Validate(); err != nil {
-		return err
-	}
-	return nil
-}
 
 type Handler struct {
 	userSvc     anor.UserService
@@ -71,16 +51,109 @@ func NewHandler(
 	}
 }
 
+func (h *Handler) Render(w http.ResponseWriter, r *http.Request, page string, data interface{}) {
+	slog.Debug("rendering page...", "name", page)
+	ctx := r.Context()
+	if isHXRequest(r) {
+		v := path.Join(page, "content.gohtml")
+		h.view.Render(w, v, data)
+		return
+	}
+
+	hc, err := h.headerContent(ctx)
+	if err != nil {
+		fmt.Println(err.Error())
+		h.serverInternalError(w, err)
+		return
+	}
+
+	v := path.Join(page, "base.gohtml")
+	switch data.(type) {
+	case homepage.Content:
+		c, _ := data.(homepage.Content)
+		base := homepage.Base{
+			Header:  hc,
+			Content: c,
+		}
+
+		h.view.Render(w, v, base)
+	case productdetailspage.Content:
+		slog.Debug("product details page content")
+		c, ok := data.(productdetailspage.Content)
+		if !ok {
+			slog.Warn("could not convert the content", "data", data)
+		}
+		base := productdetailspage.Base{
+			Header:  hc,
+			Content: c,
+		}
+		h.view.Render(w, v, base)
+	case searchlistings.Content:
+		c, _ := data.(searchlistings.Content)
+		base := searchlistings.Base{
+			Header:  hc,
+			Content: c,
+		}
+		h.view.Render(w, v, base)
+	case notfoundpage.Content:
+		c, _ := data.(notfoundpage.Content)
+		base := notfoundpage.Base{
+			Header:  hc,
+			Content: c,
+		}
+		h.view.Render(w, v, base)
+	default:
+		base := notfoundpage.Base{
+			Header: hc,
+			Content: notfoundpage.Content{
+				Message: "Page not found",
+			},
+		}
+		h.view.Render(w, v, base)
+	}
+}
+
+func (h *Handler) headerContent(ctx context.Context) (partials.Header, error) {
+	u := anor.UserFromContext(ctx)
+	header := partials.Header{User: u}
+	if u != nil {
+		ac, err := h.userSvc.GetUserActivityCounts(ctx, u.ID)
+		if err != nil {
+			return partials.Header{}, err
+		}
+
+		header.ActiveOrdersCount = ac.ActiveOrdersCount
+		header.WishlistItemsCount = ac.WishlistItemsCount
+		header.CartItemsCount = ac.CartItemsCount
+
+	} else {
+		cartId := h.session.Guest.GetInt64(ctx, "guest_cart_id")
+		if cartId != 0 {
+			guestCartItemCount, err := h.cartSvc.CountCartItems(ctx, cartId)
+			if err != nil {
+				return partials.Header{}, err
+			}
+			header.CartItemsCount = int(guestCartItemCount)
+		}
+	}
+
+	return header, nil
+}
+
 func (h *Handler) clientError(w http.ResponseWriter, err error, statusCode int) {
-	httperrors.ClientError(h.logger, w, err, statusCode)
+	anor.ClientError(h.logger, w, err, statusCode)
 }
 
 func (h *Handler) serverInternalError(w http.ResponseWriter, err error) {
-	httperrors.ServerInternalError(h.logger, w, err)
+	anor.ServerInternalError(h.logger, w, err)
 }
 
-func (h *Handler) logClientError(err error) {
-	httperrors.LogClientError(h.logger, err)
+func isHXRequest(r *http.Request) bool {
+	if r.Header.Get("Hx-Request") == "true" {
+		return true
+	}
+
+	return false
 }
 
 func calcTotalPages(total int64, perPage int) int {
